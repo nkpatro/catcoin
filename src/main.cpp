76fd0304a87cdd6fa46e8bdf0877811b870faaa5
Bradley Matusiak <bmatusiak@gmail.com>
2014-03-17 01:15:40 -0400

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ CTxMemPool mempool;
 unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
-uint256 hashGenesisBlock("0xb280d6d2829e50aeb6862ced2c8c3de3f7360e0ee2e1521b2293b10ceba5693d");
+uint256 hashGenesisBlock("0x12a765e31ffd4059bada1e25190f6e98c99d9714d334efa41a195a7e7e04bfe2");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // Diffcoin: starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
