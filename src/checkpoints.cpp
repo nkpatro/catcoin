10204f1fd476956272633e639b60c4389616990b
Bradley Matusiak <bmatusiak@gmail.com>
2014-03-17 01:08:38 -0400

    updates

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -39,7 +39,7 @@ namespace Checkpoints
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1395031246, // * UNIX timestamp of last checkpoint block
+        1395031919, // * UNIX timestamp of last checkpoint block
         0,    // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
         8000.0     // * estimated number of transactions per day after checkpoint
@@ -51,7 +51,7 @@ namespace Checkpoints
         ;
     static const CCheckpointData dataTestnet = {
         &mapCheckpointsTestnet,
-        1395031246,
+        1395031919,
         0,
         300
     };
