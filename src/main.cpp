b9494f5a9196c60a380c8ed8ad86a1a11f342686
temple <temple@9cat.net>
2014-03-24 20:33:49 -0700

    update genius

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ unsigned int nTransactionsUpdated = 0;
 
 map<uint256, CBlockIndex*> mapBlockIndex;
 //uint256 hashGenesisBlock("0xb0792b4da4974af9fec42efe585a047a78d4b005752edda0a8f08ea6f3295506");
-uint256 hashGenesisBlock("0xf4b59e55508baf1ff2634533413321bd124bfa65f841f107a59499ef6b566983");
+uint256 hashGenesisBlock("0x97252cd2a6aa87b8b20e5eda7b958d38504c5c4d367f52f77a4347112b0af18d");
 static CBigNum bnProofOfWorkLimit(~uint256(0) >> 20); // Templecoin: starting difficulty is 1 / 2^12
 CBlockIndex* pindexGenesisBlock = NULL;
 int nBestHeight = -1;
