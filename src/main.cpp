c91251573f5be3ab4f510b0d2e089791c4403abf
Bradley Matusiak <bmatusiak@gmail.com>
2014-03-27 22:59:06 -0400

    beta

--- src/main.cpp
+++ src/main.cpp
@@ -2842,7 +2842,7 @@ bool InitBlockIndex() {
         printf("hash %s\n", hash.ToString().c_str());
         printf("hashGenesisBlock %s\n", hashGenesisBlock.ToString().c_str());
         printf("block.hashMerkleRoot %s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x2b56889fb8123952057289b7091784dfde8b3acae672cc48494d2faf4348f49e"));
+        //assert(block.hashMerkleRoot == uint256("0x2b56889fb8123952057289b7091784dfde8b3acae672cc48494d2faf4348f49e"));
         // If genesis block hash does not match, then generate new genesis hash.
         if (true && hash != hashGenesisBlock)
         {
@@ -2885,6 +2885,7 @@ bool InitBlockIndex() {
             printf("block.nTime = %u \n", block.nTime);
             printf("block.nNonce = %u \n", block.nNonce);
             printf("block.GetHash = %s\n", block.GetHash().ToString().c_str());
+            printf("block.hashMerkleRoot = %s\n", block.hashMerkleRoot.ToString().c_str());
             hashGenesisBlock = block.GetHash();
             hash = block.GetHash();
         }
