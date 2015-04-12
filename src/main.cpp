93785983010a6437c864c1c632c9b10f7fc905f7
ny2cafuse <jessfoddrill@gmail.com>
2015-04-12 11:34:19 -0700

    Main.cpp- pchMessageStart change.

--- src/main.cpp
+++ src/main.cpp
@@ -3110,7 +3110,7 @@ bool static AlreadyHave(const CInv& inv)
 // The message start string is designed to be unlikely to occur in normal data.
 // The characters are rarely used upper ASCII, not valid as UTF-8, and produce
 // a large 4-byte int at any alignment.
-unsigned char pchMessageStart[4] = { 0xfb, 0xc0, 0xb6, 0xdb }; // Auroracoin: increase each by adding 2 to bitcoin's value.
+unsigned char pchMessageStart[4] = { 0xfd, 0xa4, 0xdc, 0x6c }; // Auroracoin: increase each by adding 2 to bitcoin's value.
 
 
 void static ProcessGetData(CNode* pfrom)
