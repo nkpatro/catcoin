6e12ede1d6c0ab26a0f3082277f8937a6f013c03
Jianping Wu <jianpingw@gmail.com>
2018-11-25 22:32:25 -0800

    More changes from litecoins to kevacoins.

--- src/init.cpp
+++ src/init.cpp
@@ -185,7 +185,7 @@ void Shutdown()
     /// for example if the data directory was found to be locked.
     /// Be sure that anything that writes files or flushes caches only does this if the respective
     /// module was initialized.
-    RenameThread("litecoin-shutoff");
+    RenameThread("kevacoin-shutoff");
     mempool.AddTransactionsUpdated(1);
 
     StopHTTPRPC();
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("lites");
-    case uBTC: return QString("photons");
+    case BTC: return QString("KVA");
+    case mBTC: return QString("k");
+    case uBTC: return QString("m");
     default: return QString("???");
     }
 }
@@ -60,9 +60,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Lites (1 / 1" THIN_SP_UTF8 "000)");
-    case uBTC: return QString("Photons (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
+    case BTC: return QString("Kevacoins");
+    case mBTC: return QString("k (1 / 1" THIN_SP_UTF8 "000)");
+    case uBTC: return QString("m (1 / 1" THIN_SP_UTF8 "000" THIN_SP_UTF8 "000)");
     default: return QString("???");
     }
 }
