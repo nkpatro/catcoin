0275311a3cad49f765dfcb89a9d706f36ef59212
Joshua <JoshuaBijak@gmail.com>
2019-05-08 16:14:45 +0100

    Quick FIx

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -141,7 +141,7 @@ public:
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
 
-        fMiningRequiresPeers = true;
+        //fMiningRequiresPeers = true;
         fDefaultConsistencyChecks = false;
         fRequireStandard = true;
         fMineBlocksOnDemand = false;
@@ -233,7 +233,7 @@ public:
         bech32_hrp = "tblue";
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_test, pnSeed6_test + ARRAYLEN(pnSeed6_test));
-        fMiningRequiresPeers = false;
+        //fMiningRequiresPeers = false;
         fDefaultConsistencyChecks = false;
         fRequireStandard = false;
         fMineBlocksOnDemand = false;
