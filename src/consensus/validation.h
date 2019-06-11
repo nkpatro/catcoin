456f09b63bec93f1e10d8c8043f836443060fdfa
Counos Dev <dev@counos.io>
2019-06-11 09:58:01 +0200

    Edit COINBASE_MATURITY condition

--- src/consensus/validation.h
+++ src/consensus/validation.h
@@ -106,5 +106,13 @@ static inline int64_t GetTransactionInputWeight(const CTxIn& txin)
     // scriptWitness size is added here because witnesses and txins are split up in segwit serialization.
     return ::GetSerializeSize(txin, SER_NETWORK, PROTOCOL_VERSION | SERIALIZE_TRANSACTION_NO_WITNESS) * (WITNESS_SCALE_FACTOR - 1) + ::GetSerializeSize(txin, SER_NETWORK, PROTOCOL_VERSION) + ::GetSerializeSize(txin.scriptWitness.stack, SER_NETWORK, PROTOCOL_VERSION);
 }
+static inline int64_t GetCOINBASE_MATURITY(const Coin& coin)
+{
+    // Get Coinbas maturity based on hieght of coin base.
 
+    if(coin.nHeight < COINBASE_MATURITY_FIRSTSTEP_BLOCK)
+       return FIRST_COINBASE_MATURITY;
+       else
+         return SECOND_COINBASE_MATURITY;
+}
 #endif // BITCOIN_CONSENSUS_VALIDATION_H
