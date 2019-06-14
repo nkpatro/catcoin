52b2c0c3c82f2a4c83871ff2b5b4f85b6da2c0e5
Counos Dev <dev@counos.io>
2019-06-14 08:57:40 +0200

    return change

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -112,7 +112,7 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xe1;
+        pchMessageStart[0] = 0xfb;
         pchMessageStart[1] = 0xc0;
         pchMessageStart[2] = 0xb6;
         pchMessageStart[3] = 0xdb;
