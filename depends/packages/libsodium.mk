0083dcf28e66ad21b8aeec541ffafc2f0c71eeee
Just Wonder <jianpingw@gmail.com>
2019-12-28 12:54:43 -0800

    Updated libsodium sha2 signature.

--- depends/packages/libsodium.mk
+++ depends/packages/libsodium.mk
@@ -2,7 +2,7 @@ package=libsodium
 $(package)_version=1.0.18
 $(package)_download_path=https://download.libsodium.org/libsodium/releases/
 $(package)_file_name=$(package)-$($(package)_version)-stable.tar.gz
-$(package)_sha256_hash=957445a1d9553d5b50ef36920db647077d2b9f348f060e3703db83e5a49f9803
+$(package)_sha256_hash=7047fc20e7692bbfa656a5df825983ad7c7640eaec3926331d14aa2ae1d542ea
 
 define $(package)_set_vars
 $(package)_config_opts_mingw32+=CFLAGS="-Ofast -fomit-frame-pointer -march=pentium3 -mtune=westmere"
