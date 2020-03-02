737c4923065a42099fa38c851913bbbe3f03557a
Just Wonder <jianpingw@gmail.com>
2020-03-02 12:47:43 -0800

    Used Kevacoin RandomX variant.

--- src/cn_utils/randomx/configuration.h
+++ src/cn_utils/randomx/configuration.h
@@ -28,6 +28,8 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #pragma once
 
+#define _KEVACOIN				   1
+
 //Cache size in KiB. Must be a power of 2.
 #define RANDOMX_ARGON_MEMORY       262144
 
@@ -38,7 +40,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RANDOMX_ARGON_LANES        1
 
 //Argon2d salt
+#ifdef _KEVACOIN
+#define RANDOMX_ARGON_SALT         "RandomKV\x01"
+#else
 #define RANDOMX_ARGON_SALT         "RandomX\x03"
+#endif
 
 //Number of random Cache accesses per Dataset item. Minimum is 2.
 #define RANDOMX_CACHE_ACCESSES     8
@@ -62,7 +68,11 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define RANDOMX_PROGRAM_COUNT      8
 
 //Scratchpad L3 size in bytes. Must be a power of 2.
+#ifdef _KEVACOIN
+#define RANDOMX_SCRATCHPAD_L3      1048576
+#else
 #define RANDOMX_SCRATCHPAD_L3      2097152
+#endif
 
 //Scratchpad L2 size in bytes. Must be a power of two and less than or equal to RANDOMX_SCRATCHPAD_L3.
 #define RANDOMX_SCRATCHPAD_L2      262144
