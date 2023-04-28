9401442c889cb699f38cdc344cff156ed2a05ae9
David Burkett <davidburkett38@gmail.com>
2023-04-28 15:52:01 -0004

    Litecoin: Consensus: Avoid overflow when calculating next difficulty

--- src/pow.cpp
+++ src/pow.cpp
@@ -46,6 +46,32 @@ unsigned int GetNextWorkRequired(const CBlockIndex* pindexLast, const CBlockHead
     return CalculateNextWorkRequired(pindexLast, pindexFirst->GetBlockTime(), params);
 }
 
+// Performs the difficulty retargeting calculation for the provided consensus params, previous nbits, and timespan since the last retarget.
+// NOTE: The PoW difficulty for litecoin can result in a 1 bit overflow during the retargeting calculation.
+// This function works around that issue by right shifting one bit before the calculation, then left shifting the result.
+static uint32_t CalculateAdjustedTarget(const uint32_t last_nbits, const int64_t nTimespan, const Consensus::Params& params)
+{
+    // Retarget
+    arith_uint256 bnNew;
+    bnNew.SetCompact(last_nbits);
+
+    // Litecoin: intermediate uint256 can overflow by 1 bit
+    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
+    const bool fShift = bnNew.bits() > bnPowLimit.bits() - 1;
+    if (fShift)
+        bnNew >>= 1;
+
+    bnNew *= nTimespan;
+    bnNew /= params.nPowTargetTimespan;
+    if (fShift)
+        bnNew <<= 1;
+
+    if (bnNew > bnPowLimit)
+        bnNew = bnPowLimit;
+
+    return bnNew.GetCompact();
+}
+
 unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nFirstBlockTime, const Consensus::Params& params)
 {
     if (params.fPowNoRetargeting)
@@ -59,16 +85,7 @@ unsigned int CalculateNextWorkRequired(const CBlockIndex* pindexLast, int64_t nF
         nActualTimespan = params.nPowTargetTimespan*4;
 
     // Retarget
-    const arith_uint256 bnPowLimit = UintToArith256(params.powLimit);
-    arith_uint256 bnNew;
-    bnNew.SetCompact(pindexLast->nBits);
-    bnNew *= nActualTimespan;
-    bnNew /= params.nPowTargetTimespan;
-
-    if (bnNew > bnPowLimit)
-        bnNew = bnPowLimit;
-
-    return bnNew.GetCompact();
+    return CalculateAdjustedTarget(pindexLast->nBits, nActualTimespan, params);
 }
 
 // Check that on difficulty adjustments, the new difficulty does not increase
@@ -81,41 +98,24 @@ bool PermittedDifficultyTransition(const Consensus::Params& params, int64_t heig
         int64_t smallest_timespan = params.nPowTargetTimespan/4;
         int64_t largest_timespan = params.nPowTargetTimespan*4;
 
-        const arith_uint256 pow_limit = UintToArith256(params.powLimit);
         arith_uint256 observed_new_target;
         observed_new_target.SetCompact(new_nbits);
 
-        // Calculate the largest difficulty value possible:
-        arith_uint256 largest_difficulty_target;
-        largest_difficulty_target.SetCompact(old_nbits);
-        largest_difficulty_target *= largest_timespan;
-        largest_difficulty_target /= params.nPowTargetTimespan;
-
-        if (largest_difficulty_target > pow_limit) {
-            largest_difficulty_target = pow_limit;
-        }
-
-        // Round and then compare this new calculated value to what is
-        // observed.
+        // Calculate the largest difficulty value possible.
+        // Round and then compare this new calculated value to what is observed.
         arith_uint256 maximum_new_target;
-        maximum_new_target.SetCompact(largest_difficulty_target.GetCompact());
-        if (maximum_new_target < observed_new_target) return false;
-
-        // Calculate the smallest difficulty value possible:
-        arith_uint256 smallest_difficulty_target;
-        smallest_difficulty_target.SetCompact(old_nbits);
-        smallest_difficulty_target *= smallest_timespan;
-        smallest_difficulty_target /= params.nPowTargetTimespan;
-
-        if (smallest_difficulty_target > pow_limit) {
-            smallest_difficulty_target = pow_limit;
+        maximum_new_target.SetCompact(CalculateAdjustedTarget(old_nbits, largest_timespan, params));
+        if (maximum_new_target < observed_new_target) {
+            return false;
         }
-
-        // Round and then compare this new calculated value to what is
-        // observed.
+        
+        // Calculate the smallest difficulty value possible:
+        // Round and then compare this new calculated value to what is observed.
         arith_uint256 minimum_new_target;
-        minimum_new_target.SetCompact(smallest_difficulty_target.GetCompact());
-        if (minimum_new_target > observed_new_target) return false;
+        minimum_new_target.SetCompact(CalculateAdjustedTarget(old_nbits, smallest_timespan, params));
+        if (minimum_new_target > observed_new_target) {
+            return false;
+        }
     } else if (old_nbits != new_nbits) {
         return false;
     }
