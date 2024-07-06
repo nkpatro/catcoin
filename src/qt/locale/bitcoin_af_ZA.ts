3f43ae478eb0aa29e1bb96801809b32c639e5c3f
James Luiz <jamesluiz116@gmail.com>
2024-07-06 11:54:10 -0400

    tapcoin details added

--- src/qt/locale/bitcoin_af_ZA.ts
+++ src/qt/locale/bitcoin_af_ZA.ts
@@ -214,8 +214,8 @@
         <translation>&amp;Opsies</translation>
     </message>
     <message>
-        <source>Litecoin</source>
-        <translation>Litecoin</translation>
+        <source>Tapcoin</source>
+        <translation>Tapcoin</translation>
     </message>
     <message>
         <source>Wallet</source>
--- src/qt/locale/bitcoin_sr.ts
+++ src/qt/locale/bitcoin_sr.ts
@@ -130,8 +130,8 @@
         <translation>Промени &amp;лозинку...</translation>
     </message>
     <message>
-        <source>Send coins to a Litecoin address</source>
-        <translation>Пошаљите новац на litecoin адресу</translation>
+        <source>Send coins to a Tapcoin address</source>
+        <translation>Пошаљите новац на tapcoin адресу</translation>
     </message>
     <message>
         <source>Change the passphrase used for wallet encryption</source>
